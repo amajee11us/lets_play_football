b34fad1f32c140e1f1952f9eee029f12fe2ced3a
amajee11us <anaymajee11us@gmail.com>
2021-01-11 01:55:39 +0530

    WIP: Train script for training the PPO model

--- train_ppo.py
+++ train_ppo.py
@@ -1,11 +1,13 @@
 import os
 import torch
+from torch.distributions import Normal
 import numpy as np
 import gfootball.env as football_env
+import gym
 
 from models.actor import Actor
 from models.critic import Critic
-
+from models.ActorCritic import ActorCritic
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -14,7 +16,7 @@ if __name__ == '__main__':
     env = football_env.create_environment(env_name='academy_empty_goal', representation='pixels', render=True)
     state = env.reset()
 
-    state_dims = env.observation_space.shape 
+    state_dims = env.observation_space.shape
     print(state_dims)
 
     n_actions = env.action_space.n 
@@ -28,108 +30,56 @@ if __name__ == '__main__':
     masks = []
     rewards = []
     actions_probs = []
-    actions_onehot = []
+    entropy = 0
 
-    actor = Actor(n_actions, 0.002, (0.9, 0.999))
-    m_actor = actor.get_model()
+    model = ActorCritic(n_actions, 0.002, (0.9, 0.999))
+    model.to(device)
+    model.eval()
 
-    critic = Critic(0.002, (0.9, 0.999))
-    m_critic = critic.get_model()
+    optim = torch.optim.Adam(model.parameters(), lr=0.002)
 
-    # evvauation
-    m_actor.to(device)
-    m_critic.to(device)
-    m_critic.eval()
-    m_actor.eval()
-
-    print(m_actor)
-    print(m_critic)
+    print(model)    
 
     for itr in range(ppo_steps):
         state = torch.from_numpy(state).float().to(device)
         state = torch.unsqueeze(state, 0)
         state = state.permute(0, 3, 1, 2)
         print(state.shape)
-
-        with torch.no_grad():
-            action_dist = m_actor(state)
-            q_value = m_critic(state)
         
-        print(action_dist.shape)
-
-        action_dist = action_dist.cpu().numpy()
-        q_value = q_value.cpu().numpy()
-
-        action = np.random.choice(n_actions, p=action_dist[0, :])
-        action_onehot = np.zeros(n_actions)
-        action_onehot[action] = 1 # set  value of onehot vector
-
+        # jointly forward pass on actor and critic models
+        action_dist, q_value = model(state)
+                     
         # Perform an action
-        observation, reward, done, info = env.step(action)
+        action = action_dist.sample()
+        print(action)
+        next_state, reward, done, info = env.step(action.item())
+
+        log_probs = action_dist.log_prob(action)
+        entropy += action_dist.entropy().mean()
+        print(entropy)
+        
+        print(" Iter : {} State shape : {} Reward: {} Mask : {} ".format(itr, state.shape, reward, done))
+
+        '''
+        # NOTE : only if multiple envs are present
+        reward = torch.FloatTensor(reward).unsqueeze(1).to(device)
         mask = torch.FloatTensor(1 - done).unsqueeze(1).to(device)
+        '''
+        
+        reward = torch.FloatTensor([reward])
+        mask = torch.FloatTensor([1 - done])
 
         states.append(state)
         actions.append(action)
-        actions_onehot.append(action_onehot)
+        actions_probs.append(log_probs)
         values.append(q_value)
         masks.append(mask)
-        rewards.append(torch.FloatTensor(reward).unsqueeze(1).to(device))
+        rewards.append(reward)
         actions_probs.append(action_dist)
 
-        state = observation # update to next state
+        state = next_state # update to next state
 
         if done:
             env.reset()
 
     env.close()
-
-def calculate_advantage(masks, q_values, rewards):
-    gae = 0
-    returns_per_state = [] # this is tracked per step to achieve a goal
-
-    for i in reversed(range(len(rewards))):
-        delta = rewards[i] + (gamma * q_values[i + 1] * masks[i]) - values[i]
-        gae = delta + gamma * lmbda * masks[i] * gae
-        returns_per_state.insert(0, gae + values[i])
-    
-    adv = np.array(returns_per_state) - q_values[:-1]
-    return returns_per_state, (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
-
-class PPO:
-
-    def __init__(self, learn_rate= 0.01, betas= (0.9, 0.999), eps_clip=0.2, gamma=0.99, lmbda=0.95):
-        # Advantage calculation
-        self.gamma = gamma # discount factor
-        self.lmbda = lmbda # smoothing factor
-
-        #PPO loss constants
-        self.lr = learn_rate
-        self.betas = betas
-        self.eps_clip = eps_clip
-
-        self.MSEloss = torch.nn.MSELoss()
-
-    def calc_advantage(self, masks, q_values, rewards):
-        '''
-        Genearlized Advantage Estimation Algorithm
-        '''
-        gae = 0
-        returns_per_state = [] # this is tracked per step to achieve a goal
-
-        for i in reversed(range(len(rewards))):
-            delta = rewards[i] + (self.gamma * q_values[i + 1] * masks[i]) - values[i]
-            gae = delta + self.gamma * self.lmbda * masks[i] * gae
-            returns_per_state.insert(0, gae + values[i])
-        
-        adv = np.array(returns_per_state) - q_values[:-1]
-        return returns_per_state, (adv - np.mean(adv)) / (np.std(adv) + 1e-10)
-    
-    def _calc_entropy(self, state_values):
-        '''
-        Calculate the entropy of the state predictions
-        '''
-        entropy = torch.mean(state * torch.log(state + 1e-10))
-        return entropy
-
-    def calc_loss(self):
-
